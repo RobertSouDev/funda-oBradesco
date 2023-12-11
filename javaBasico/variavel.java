e1f07a84dbfb5670ed17ecada52412b83d2e2404
RobertSouDev <rogerhallks1@gmail.com>
2023-12-10 22:35:59 -0300

    Variaveis Java

---
+++ javaBasico/variavel.java
@@ -0,0 +1,11 @@
+public class variavel {
+    // metodo principal da classe
+    public static void main(String[] args){
+        String nome = "Robert";
+        int idade = 25;
+        boolean casado = true;
+        // imprime  em tela  o resultado  de nossas variaveis
+        System.out.println(nome);;
+
+    }// fim do metodo
+}// fim da classe
