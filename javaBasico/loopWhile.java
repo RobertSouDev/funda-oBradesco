86f75cc669ba4026dcd554b5abccfda6d7bbf786
RobertSouDev <rogerhallks1@gmail.com>
2023-12-12 01:00:28 -0300

    loop while

---
+++ javaBasico/loopWhile.java
@@ -0,0 +1,9 @@
+public class loopWhile {
+    public static void main (String[]args){
+        int var = 1;
+        while (var < 5){
+            System.out.println(var);
+            ++var;
+        }
+    }
+}
