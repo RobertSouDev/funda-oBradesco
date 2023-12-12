14490908a4411b5906aa1c9d562c79bbfae1517b
RobertSouDev <rogerhallks1@gmail.com>
2023-12-12 00:40:33 -0300

    if/else

---
+++ javaBasico/condicional.java
@@ -0,0 +1,8 @@
+public class condiconal { //controle de fluxo
+    public static void main (String []args) {
+        int idade = 8;
+        if (idade < 18) {
+            System.out.println("Ainda e uma criança");
+        }
+    }
+}
---
+++ javaBasico/operador.java
@@ -0,0 +1,7 @@
+public class Operador {
+    // metodo  principal classe
+    public static void main (String[] args) {
+        double x = 7 * 3;
+        System.out.println("x")
+    }
+}
