b926f0573c37c2a5474a0836d29490e5e1e4ff8c
RobertSouDev <rogerhallks1@gmail.com>
2023-12-12 00:54:45 -0300

    if/if else/else

--- javaBasico/condicional.java
+++ javaBasico/condicional.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class condiconal { //controle de fluxo
     public static void main (String []args) {
-        int idade = 8;
-        if (idade < 18) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite a idade: ");
+
+        int idade = sc.nextInt(); // Recebe a entrada do usuário sem argumentos
+
+        if (idade < 12) {
+
             System.out.println("Ainda e uma criança");
+        }else if (idade < 18) {
+            System.out.println("e adolencente");
+        } else {
+            System.out.println("e de maior");
+
         }
     }
 }
